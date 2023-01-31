a0c3a0a22b21e0508ff3c77afcf01d4c3ee8a536
JackyLai327 <jackylai327@gamil.com>
2023-01-31 22:00:06 +1100

    added pages structures v0

---
+++ app/src/pages/Education.js
@@ -0,0 +1,12 @@
+function Education() {
+  return (
+    <div>
+      <h1>Education</h1>
+      <p>Here is my education/skills</p>
+      <p>awards can go in here</p>
+      <p>maybe notable classes too</p>
+    </div>
+  )
+}
+
+export default Education;
---
+++ app/src/pages/Experience.js
@@ -0,0 +1,10 @@
+function Experience() {
+    return (
+        <div>
+        <h1>Experience</h1>
+        <p>My experience</p>
+        </div>
+    );
+}
+
+export default Experience;
