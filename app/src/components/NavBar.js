dccb54bcfef1cd8f712cd525dd59ecfdddaa9a73
JackyLai327 <jackylai327@gamil.com>
2023-02-05 17:17:37 +1100

    Home page v1 uploaded

--- app/src/components/NavBar.js
+++ app/src/components/NavBar.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 function NavBar() {
+    const location = window.location.pathname;
 
     const [navOpen, setNavOpen] = useState(false);
 
@@ -15,12 +16,12 @@ function NavBar() {
     return (
         <div>
             <div className="nav-bar" onMouseLeave={handleNavClose}>
-                <div className="nav-bar-item"><a href="/">Home</a></div>
-                <div className="nav-bar-item about-btn" onMouseEnter={handleNavOpen}>About</div>
+                <div className={location === "/" ? "nav-bar-item user-location" : "nav-bar-item"}><a href="/">Home</a></div>
+                <div className={location === "/education" || location === "/experience" || location === "/projects" ? "nav-bar-item about-btn user-location" : "nav-bar-item about-btn"} onMouseEnter={handleNavOpen}>About</div>
                 <div className={navOpen === true ? "nav-bar-item nav-bar-open" : "nav-bar-closed"}><a href="/education">Education</a></div>
                 <div className={navOpen === true ? "nav-bar-item nav-bar-open" : "nav-bar-closed"}><a href="/experience">Experience</a></div>
                 <div className={navOpen === true ? "nav-bar-item nav-bar-open" : "nav-bar-closed"}><a href="/projects">Projects</a></div>
-                <div className={navOpen === true ? "nav-bar-item contact-pushed" : "nav-bar-item contact-back"}><a href="/contact">Contact</a></div>
+                <div className={navOpen === true ? "nav-bar-item contact-pushed" : "nav-bar-item contact-back"}><a className = {location === "/contact" ? "user-location" : ''} href="/contact">Contact</a></div>
                 <div className={navOpen === true ? "nav-bar-underline-open" : "nav-bar-underline-closed"}></div>
             </div>
         </div>
--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
+import profilePic from '../resources/profile_pic.jpeg';
 
 function Home() {
 
   return (
     <div>
-      <div className='home-top-block'></div>
+      <div className='home-top-block'>
+        <div className='summary'>
+          <div className='code-font typewriter'>
+            <span className='text-function-color'>print</span>
+            <span className='text-parenthesis-color'>(</span>
+            <span className='text-string-color'>'Hello, world!'</span>
+            <span className='text-parenthesis-color'>)</span>
+          </div>
+          <div>My name is Yu-Cheng Lai</div>
+          <div>I'm an IT student at RMIT</div>
+        </div>
+      </div>
       
+
       <NavBar />
 
-      <div className='text-danger'>PHOTO AND SUMMARY</div>
-
-      <Footer />
+      <div className='home-pfp'>
+        <img src={profilePic} alt='pfp' /> 
+      </div>
     </div>
   );
 }
