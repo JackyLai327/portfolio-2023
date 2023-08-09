36462bb548bd0bc36717203ac931ebd2d790e837
JackyLai327 <jackylai327@gamil.com>
2023-08-09 23:52:03 +1000

    router fix -7

--- app/src/components/AppRouter.js
+++ app/src/components/AppRouter.js
@@ -4,11 +4,11 @@ import Education from '../pages/Education';
 import Experience from '../pages/Experience';
 import Projects from '../pages/Projects';
 import Contact from '../pages/Contact';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, HashRouter } from 'react-router-dom';
 
 function AppRouter() {
     return (
-        <Router basename='/portfolio-2023/'>
+        <HashRouter basename='/portfolio-2023/'>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/education/" element={<Education />} />
@@ -16,7 +16,7 @@ function AppRouter() {
                 <Route path="/projects/" element={<Projects />} />
                 <Route path="/contact/" element={<Contact />} />
             </Routes>
-        </Router>
+        </HashRouter>
     )
 }
 
