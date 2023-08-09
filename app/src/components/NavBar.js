cb595d8bfcb38ba155eff01a6ae83ccb2fd123d7
JackyLai327 <jackylai327@gamil.com>
2023-08-10 00:33:24 +1000

    router fix -17

--- app/src/components/NavBar.js
+++ app/src/components/NavBar.js
@@ -17,13 +17,12 @@ function NavBar() {
     return (
         <>
             <div className="nav-bar" onMouseLeave={handleNavClose}>
-                <div className={location === "/" ? "nav-bar-item user-location" : "nav-bar-item"}><a href="/portfolio-2023/">Home</a></div>
-                <div className={location === "/portfolio-2023/education/" || location === "/portfolio-2023/experience/" || location === "/portfolio-2023/projects/" ? "nav-bar-item about-btn user-location" : "nav-bar-item about-btn"} onMouseEnter={handleNavOpen}>About</div>
-                <div className={navOpen === true ? "nav-bar-item nav-bar-open" : "nav-bar-closed"}><a href="/portfolio-2023/education/">Education</a></div>
-                <Link to="/experience/"><div className={navOpen === true ? "nav-bar-item nav-bar-open" : "nav-bar-closed"}>Experience</div></Link>
-                {/* <div className={navOpen === true ? "nav-bar-item nav-bar-open" : "nav-bar-closed"}><a href="/portfolio-2023/experience/">Experience</a></div> */}
-                <div className={navOpen === true ? "nav-bar-item nav-bar-open" : "nav-bar-closed"}><a href="/portfolio-2023/projects/">Projects</a></div>
-                <div className={navOpen === true ? "nav-bar-item contact-pushed" : "nav-bar-item contact-back"}><a className = {location === "/portfolio-2023/contact/" ? "user-location" : ''} href="/portfolio-2023/contact/">Contact</a></div>
+                <div className={location === "/" ? "nav-bar-item user-location" : "nav-bar-item"}><Link to={"/"}>Home</Link></div>
+                <div className={location === "/education/" || location === "/experience/" || location === "/projects/" ? "nav-bar-item about-btn user-location" : "nav-bar-item about-btn"} onMouseEnter={handleNavOpen}>About</div>
+                <div className={navOpen === true ? "nav-bar-item nav-bar-open" : "nav-bar-closed"}><Link to={"/education/"}>Education</Link></div>
+                <div className={navOpen === true ? "nav-bar-item nav-bar-open" : "nav-bar-closed"}><Link to={"/experience/"}>Experience</Link></div>
+                <div className={navOpen === true ? "nav-bar-item nav-bar-open" : "nav-bar-closed"}><Link to={"/projects/"}>Projects</Link></div>
+                <div className={navOpen === true ? "nav-bar-item contact-pushed" : "nav-bar-item contact-back"}><Link to={"/contact/"} className = {location === "/contact/" ? "user-location" : ''}>Contact</Link></div>
             </div>
             <div className={navOpen === true ? "nav-bar-underline-open" : "nav-bar-underline-closed"}></div>
             <div className='line-across'></div>
