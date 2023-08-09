a1422bf44fcc6c1b82d9d81514af391e69519351
JackyLai327 <jackylai327@gamil.com>
2023-08-10 00:16:44 +1000

    router fix -12

--- app/src/index.js
+++ app/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import AppRouter from './AppRouter';
+import AppRouter from './components/AppRouter';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
